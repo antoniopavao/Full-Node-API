b2189b458a29babb6cf4d67f14597d938476ce1d
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-02-12 11:57:08 -0300

    Creating controllers and routes

---
+++ src/mocks/products.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Coca Cola Lata"
+    },
+        {
+        id: 2,
+        name: "Biscoito Trakinas"
+    },
+        {
+        id: 3,
+        name: "Bolacha Passatempo"
+    },
+]
