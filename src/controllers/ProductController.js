5a0d76d6161bec019246c8e13fdf661d7a49c6cb
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-02-12 12:34:40 -0300

    accepting query Params

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,7 +2,21 @@ const products = require("../mocks/products");
 
 module.exports = {
     listProducts(request, response) {
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.end(JSON.stringify(products));
+
+        const {
+            order
+        } = request.query;
+
+        const sortedProducts = products.sort((a, b) => {
+            if (order === 'desc') {
+                return a.id < b.id ? 1 : -1;
+            }
+            return a.id > b.id ? 1 : -1;
+
+        })
+        response.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        response.end(JSON.stringify(sortedProducts));
     }
 }
